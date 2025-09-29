ac5ae6b0899c3a4abf58b65a1470e158bf290b12
Jeevan Reddy Y <jeevanreddy05462@gmail.com>
2025-09-29 17:59:33 +0530

    test lint-staged 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig(
     },
     rules: {
       'no-console': 'off',
+      'dot-notation': 'error',
     },
   },
   {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 function welcome(name: string) {
   console.log('Welcome, ' + name);
+
+  const user = {
+    name: 'Reddy',
+  };
+
+  const firstName = user.name;
+
+  return name + firstName;
 }
 
 welcome('Jeevan Reddy');
